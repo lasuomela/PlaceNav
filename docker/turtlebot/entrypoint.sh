fabd82d932d9d314eae7cbb49cefff0d80605b10
Lauri <lauri.a.suomela@student.tut.fi>
2024-09-27 11:24:40 +0300

    Update docker entrypoint to build viz_msgs when running for the first time

--- docker/turtlebot/entrypoint.sh
+++ docker/turtlebot/entrypoint.sh
@@ -4,5 +4,12 @@ set -e
 # setup ros environment
 source "/opt/ros/noetic/setup.bash" 
 source "/opt/turtlebot_ws/devel/setup.bash"
+
+# Check if the viz_msgs package has been built. If not, build it.
+if [ ! -d "/opt/placenav/src/placenav_viz_msgs/devel" ]; then
+    echo "Building placenav_viz_msgs package..."
+    cd /opt/placenav/src/placenav_viz_msgs
+    catkin_make
+fi
 source "/opt/placenav/src/placenav_viz_msgs/devel/setup.bash"
 exec "$@"
